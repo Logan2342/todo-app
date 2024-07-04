8f8a460c954b90d14264f41168f50916708063ab
Logan2342 <bsftio28@gmail.com>
2024-07-03 20:03:46 -0700

    Add Footer component
    A new Footer component has been created and added to the components directory. This component includes a footer element styled with Tailwind CSS.

---
+++ src/components/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+        <div className='mx-auto w-full'>
+            <footer className='w-full border-soild border-red-300 border-8'></footer>
+        </div>
+    )
+}
+
+export default Footer;
+
